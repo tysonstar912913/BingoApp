5551e6ee7e37530436711f892d174c27186bb21a
tysonstar912913 <tyson.star.912913@gmail.com>
2021-10-18 23:52:21 +0900

    complete

--- src/Bingo.js
+++ src/Bingo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Reward from "react-rewards";
 import shuffle from "shuffle-array";
 import { css, StyleSheet } from 'aphrodite';
+import { ProgressBar, Row, Col } from 'react-bootstrap';
 import {
     tada, headShake, bounce, swing, jello, flip, rollIn, rollOut,
     shake, rotateIn, bounceOutLeft, hinge, rotateOut,
@@ -81,50 +82,109 @@ function Tile({ id, children, onToggle, isSet }) {
     );
 }
 
+let tick_interval = null;
+
 class Bingo extends Component {
     constructor(props) {
         super(props);
         const dimension = 5;
         this.state = {
-            app_title: "BINGO",
             dimension: dimension,
             center_id: Math.round(dimension * dimension / 2) - 1,
+            checked: {},
+            range: [0, 1, 2, 3, 4],
+            max_selectable_time: 15,
             data: null,
             won: false,
             randomAnimation: "",
             win_title_visibility: false,
-            checked: {}
+            delay_time: 0,
+            selected_card: null,
         }
-        this.state.checked = { ...this.state.checked, [this.state.center_id]: true };
-        this.state = { ...this.state, data: this.generate_random_array() };
     }
 
-    generate_random_array = () => {
-        const bbb = [];
-        for (let index = 1; index < 100; index++) {
-            bbb.push(index);
-        }
-        const random_array = shuffle(bbb).slice(0, this.state.dimension * this.state.dimension);
-        const rand_data = random_array.reduce(
-            (data, value, index) => ({ ...data, [index]: value }),
-            {}
-        );
-        return rand_data;
-    }
-
-    restart_game = () => {
-        this.setState(state => {
-            const checked = { [state.center_id]: true }
+    init = () => {
+        this.setState((prevState) => {
+            const checked = { [prevState.center_id]: true };
+            let { rand_data, random_array } = this.generate_random_array();
+            random_array.splice(this.state.center_id, 1);
             return {
-                ...state,
-                checked: checked,
-                data: this.generate_random_array(),
-                win_title_visibility: false
+                ...this.state,
+                checked,
+                data: rand_data,
+                selectable_numbers: random_array,
+                won: false,
+                randomAnimation: "",
+                win_title_visibility: false,
+                delay_time: 0,
+                selected_card: this.get_rand_card(random_array),
             }
+        }, () => {
+            if (tick_interval !== null) {
+                clearInterval(tick_interval);
+            }
+            this.start_tick();
         })
     }
 
+    componentDidMount() {
+        this.init();
+    }
+
+    get_rand_card = (selectable_numbers) => {
+        let randomNumber = Math.floor(
+            Math.random() * (selectable_numbers.length)
+        );
+        let selected_number = selectable_numbers[randomNumber];
+        return selected_number;
+    }
+
+    generate_random_array = () => {
+        let bbb = [];
+        for (let index = 1; index < 100; index++) {
+            bbb.push(index);
+        }
+        let random_array = shuffle(bbb).slice(0, this.state.dimension * this.state.dimension);
+        let rand_data = random_array.reduce(
+            (data, value, index) => ({ ...data, [index]: value }),
+            {}
+        );
+        return {
+            random_array: random_array,
+            rand_data: rand_data
+        };
+    }
+
+    start_tick = () => {
+        if (tick_interval !== null) {
+            this.setState({
+                delay_time: 0,
+                selected_card: this.get_rand_card(this.state.selectable_numbers)
+            })
+            clearInterval(tick_interval);
+        }
+
+        tick_interval = setInterval(() => {
+            this.setState(prevState => {
+                let state = prevState;
+                if (state.delay_time >= 100) {
+                    state.delay_time = 0;
+                    state.selected_card = this.get_rand_card(state.selectable_numbers);
+                }
+                else {
+                    state.delay_time += 100 / state.max_selectable_time;
+                }
+                return {
+                    ...state
+                }
+            })
+        }, 1000)
+    }
+
     handleAnimation = () => {
+        if (tick_interval !== null) {
+            clearInterval(tick_interval);
+        }
         if (!this.state.randomAnimation) {
             const animations = [
                 "wobble", "tada", "headShake",
@@ -144,16 +204,12 @@ class Bingo extends Component {
                     Math.random() * (animations.length)
                 );
                 const randomAnimation = animations[randomNumber];
-                this.setState(prevState => ({
-                    randomAnimation: randomAnimation,
-                }))
+                this.setState({ randomAnimation: randomAnimation });
+
             }, 750)
             setTimeout(() => {
                 clearInterval(danceInterval);
-                this.setState({
-                    randomAnimation: "",
-                })
-                this.restart_game();
+                this.init();
             }, 5000);
         }
     }
@@ -163,54 +219,71 @@ class Bingo extends Component {
         this.handleAnimation();
     }
 
+    isWon = (checked) => {
+        let range = this.state.range;
+        return (
+            undefined !==
+            range.find(row => range.every(column => checked[row * this.state.dimension + column])) ||
+            undefined !==
+            range.find(column => range.every(row => checked[row * this.state.dimension + column])) ||
+            range.every(index => checked[index * this.state.dimension + index]) ||
+            range.every(index => checked[index * this.state.dimension + 4 - index])
+        );
+    };
+
     render() {
-        const isWon = (checked) => {
-            const range = [0, 1, 2, 3, 4];
-            return (
-                undefined !==
-                range.find(row => range.every(column => checked[row * this.state.dimension + column])) ||
-                undefined !==
-                range.find(column => range.every(row => checked[row * this.state.dimension + column])) ||
-                range.every(index => checked[index * this.state.dimension + index]) ||
-                range.every(index => checked[index * this.state.dimension + 4 - index])
-            );
-        };
+        let is_free_item = false;
+        const { randomAnimation, won, win_title_visibility, data, delay_time, range, selected_card } = this.state;
         const toggle = id => {
             if (parseInt(id) !== this.state.center_id) {
-                this.setState(state => {
-                    const checked = { ...this.state.checked, [id]: !this.state.checked[id] };
-                    const won = isWon(checked);
-                    if (won) {
-                        this.you_are_winner();
-                    }
-                    return {
-                        ...state,
-                        checked,
-                        won
+                this.setState(prevState => {
+                    let state = prevState;
+                    if (state.data[id] === state.selected_card) {
+                        const checked = { ...state.checked, [id]: !state.checked[id] };
+                        let selectable_numbers = state.selectable_numbers;
+                        const remove_index = selectable_numbers.indexOf(state.selected_card);
+                        if (remove_index > -1) {
+                            selectable_numbers.splice(remove_index, 1);
+                        }
+                        this.start_tick();
+                        const won = this.isWon(checked);
+                        if (won) {
+                            this.you_are_winner();
+                        }
+                        return {
+                            ...state,
+                            checked,
+                            won,
+                            selectable_numbers: selectable_numbers,
+                        }
                     }
                 })
             }
         }
-        let map = Array.prototype.map;
-        let is_free_item = false;
-        const { randomAnimation, won, win_title_visibility } = this.state;
         return (
             <div className="App">
-                <h1 className="app_title">Bingo App</h1>
+                <Row>
+                    <Col md="12">
+                        <h1 className="app_title">Bingo Game</h1>
+                    </Col>
+                </Row>
+                <Row className="rand_card">
+                    <Col>
+                        <h3>Here is the card : {selected_card}</h3>
+                    </Col>
+                </Row>
+                <Row className="progress_bar">
+                    <Col md="12" className="padding-0">
+                        <ProgressBar striped animated now={delay_time} variant="success" />
+                    </Col>
+                </Row>
                 <h1 className={`
                     win_title 
                     ${!win_title_visibility ? 'display-hide' : 'display-show'} 
                     ${css(won && randomAnimation && styles[randomAnimation])}
                     `}>You are winner!</h1>
                 <div className="wrapper">
-                    {map.call(this.state.app_title, function (x) {
-                        return (
-                            <Tile key={x}>
-                                {x}
-                            </Tile>
-                        );
-                    })}
-                    {this.state.data === null ? '' : Object.keys(this.state.data).map(id => {
+                    {data === null ? '' : Object.keys(data).map(id => {
                         if (this.state.center_id !== parseInt(id)) {
                             is_free_item = false;
                         }
